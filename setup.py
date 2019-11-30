8566dd2f040a5f851cf8d7ae231d3df485c88082
Andreas Lordos <andreasglordos@gmail.com>
2019-11-30 19:35:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@
 from setuptools import setup, find_packages
 
 
-version = '0.12.0'
+version = '0.13.0'
 
 setup(
     name='broadlink',
     version=version,
-    author='Matthew Garrett',
-    author_email='mjg59@srcf.ucam.org',
-    url='http://github.com/mjg59/python-broadlink',
+    author='Andreas Lordos',
+    author_email='andreasnohacks@gmail.com',
+    url='http://github.com/andreaslordos/python-broadlink',
     packages=find_packages(),
     scripts=[],
     install_requires=['cryptography>=2.1.1', 'PyCRC'],
